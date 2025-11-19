8f59c71ff6464fbac1afaf5861c93910bdbc51b0
Artur Ganzha <arturganzha10@gmail.com>
2025-11-19 21:46:24 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,295 @@
+use bevy::prelude::*;
+use bevy::math::bounding::{
+    Aabb2d,
+    BoundingVolume,
+    IntersectsVolume
+};
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Collision {
+    Left,
+    Right,
+    Top,
+    Bottom,
+}
+
+#[derive(Component, Default)]
+struct Collider(Rectangle);
+
+#[derive(Default, Component)]
+#[require(Transform)]
+struct Position(Vec2);
+
+
+#[derive(Component, Default)]
+struct Velocity(Vec2);
+
+#[derive(Component)]
+#[require(
+    Position, 
+    Velocity=Velocity(Vec2::new(BALL_SPEED, 0.0)),
+    Collider = Collider(Rectangle::new(BALL_SIZE, BALL_SIZE)),
+)]
+struct Ball;
+const BALL_SIZE: f32 = 10.0;
+const BALL_SHAPE: Circle = Circle::new(BALL_SIZE);
+const BALL_COLOR: Color = Color::srgb(1.0, 1.0, 1.0);
+const BALL_SPEED: f32 = 2.0;
+
+
+#[derive(Component)]
+#[require(
+    Position,
+    Collider = Collider(Rectangle::new(PADDLE_WIDTH, PADDLE_HEIGHT))
+)]
+struct Paddle;
+const PADDLE_WIDTH: f32 = 20.0;
+const PADDLE_HEIGHT: f32 = 150.0;
+const PADDLE_COLOR: Color = Color::srgb(1.0, 1.0, 1.0);
+
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Component)]
+struct Ai;
+
+#[derive(Component)]
+#[require(Position, Collider)]
+struct Gutter;
+const GUTTER_COLOR: Color = Color::srgb(0., 0., 1.);
+const GUTTER_HEIGHT: f32 = 20.;
+
+// Camera, Ball
+fn spawn_ball(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let mesh = meshes.add(BALL_SHAPE);
+    let material = materials.add(ColorMaterial::from(BALL_COLOR));
+
+    commands.spawn((
+        Ball,
+        Position(Vec2::ZERO),           // starting logical position
+        Transform::default(),           // actual transform in the world
+        Mesh2d(mesh),
+        MeshMaterial2d(material),
+    ));
+}
+
+fn camera_setup(mut commands: Commands){
+    commands.spawn(Camera2d);
+}
+
+fn project_positions(mut positionables: Query<(&mut Transform, &Position)>){
+    for (mut transform, position) in &mut positionables {
+        transform.translation = position.0.extend(0.);
+    }
+}
+
+fn move_ball(ball: Single<(&mut Position, &Velocity), With<Ball>>){
+    let (mut position, velocity) = ball.into_inner();
+    position.0 += velocity.0 * BALL_SPEED;
+}
+
+fn spawn_paddles(
+    mut commands: Commands, 
+    mut meshes: ResMut<Assets<Mesh>>, 
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    window: Single<&Window>,
+){
+    let paddle_shape = Rectangle::new(PADDLE_WIDTH, PADDLE_HEIGHT);
+    let mesh = meshes.add(paddle_shape);
+    let material = materials.add(PADDLE_COLOR);
+    let half_window_size = window.resolution.size()/2.;
+    let padding = 20.0;    
+    let player_position = Vec2::new(-half_window_size.x + padding, 0.0);
+
+    commands.spawn((
+        Player,
+        Paddle, 
+        Mesh2d(mesh.clone()), 
+        MeshMaterial2d(material.clone()), 
+        Position(player_position),
+        Velocity(Vec2::ZERO),
+    ));
+
+    let ai_position = Vec2::new(half_window_size.x - padding, 0.0);
+    commands.spawn((
+        Ai,
+        Paddle,
+        Mesh2d(mesh.clone()),
+        MeshMaterial2d(material.clone()),
+        Position(ai_position),
+        Velocity(Vec2::ZERO),
+    ));
+    }
+
+//todo: fn score
+//todo: gutter
+fn spawn_gutters(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    window: Single<&Window>,
+){
+    let material = materials.add(GUTTER_COLOR);
+    let padding = 20.0;
+    let gutter_shape = Rectangle::new(window.resolution.width(), GUTTER_HEIGHT);
+    let mesh = meshes.add(gutter_shape);
+
+    let top_gutter_position = Vec2::new(0.0, window.resolution.height() / 2.0 - padding);
+
+    commands.spawn((
+        Gutter,
+        Mesh2d(mesh.clone()),
+        MeshMaterial2d(material.clone()),
+        Position(top_gutter_position),
+        Collider(gutter_shape)
+    ));
+    let bottom_gutter_position = Vec2::new(0.0, -window.resolution.height()/2.0+padding);
+
+    commands.spawn((
+        Gutter,
+        Mesh2d(mesh.clone()),
+        MeshMaterial2d(material.clone()),
+        Position(bottom_gutter_position),
+        Collider(gutter_shape),
+    ));
+}
+
+fn collide_with_side(ball: Aabb2d, wall: Aabb2d) -> Option<Collision> {
+    if !ball.intersects(&wall){
+        return None;
+    }
+    let closest_point = wall.closest_point(ball.center());
+    let offset = ball.center() - closest_point;
+
+    let side = if offset.x.abs() > offset.y.abs(){
+        if offset.x < 0.0 {
+            Collision::Left
+        } else {
+            Collision::Right
+        }
+    } else if offset.y > 0.0 {
+        Collision::Top
+    } else {
+        Collision::Bottom
+    };
+
+    Some(side)
+}
+
+impl Collider{
+    fn half_size(&self) -> Vec2 {
+        self.0.half_size
+    }
+}
+
+fn handle_collisions(
+    ball: Single<(&mut Velocity, &Position, &Collider), With<Ball>>,
+    other_things: Query<(&Position, &Collider), Without<Ball>>,
+  ) {
+    let (mut ball_velocity, ball_position, ball_collider) = ball.into_inner();
+  
+    for (other_position, other_collider) in &other_things {
+      if let Some(collision) = collide_with_side(
+        Aabb2d::new(ball_position.0, ball_collider.half_size()),
+        Aabb2d::new(other_position.0, other_collider.half_size()),
+      ) {
+        match collision {
+          Collision::Left => {
+            ball_velocity.0.x *= -1.;
+          }
+          Collision::Right => {
+            ball_velocity.0.x *= -1.;
+          }
+          Collision::Top => {
+            ball_velocity.0.y *= -1.;
+          }
+          Collision::Bottom => {
+            ball_velocity.0.y *= -1.;
+          }
+        }
+      }
+    }
+  }
+
+
+const PADDLE_SPEED: f32 = 5.;
+fn handle_player_input(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut paddle_velocity: Single<&mut Velocity, With<Player>>,
+) {
+    if keyboard_input.pressed(KeyCode::ArrowUp) {
+        paddle_velocity.0.y = PADDLE_SPEED;
+    } else if keyboard_input.pressed(KeyCode::ArrowDown) {
+        paddle_velocity.0.y = -PADDLE_SPEED;
+    } else {
+        paddle_velocity.0.y = 0.0;
+    }
+}
+
+fn move_paddles(mut paddles: Query<(&mut Position, &Velocity), With<Paddle>>) {
+    for (mut position, velocity) in &mut paddles {
+      position.0 += velocity.0;
+    }
+}
+
+fn constrain_paddle_position(
+    mut paddles: Query<
+      (&mut Position, &Collider),
+      (With<Paddle>, Without<Gutter>),
+    >,
+    gutters: Query<(&Position, &Collider), (With<Gutter>, Without<Paddle>)>,
+  ) {
+    for (mut paddle_position, paddle_collider) in &mut paddles {
+      for (gutter_position, gutter_collider) in &gutters {
+        let paddle_aabb =
+          Aabb2d::new(paddle_position.0, paddle_collider.half_size());
+        let gutter_aabb =
+          Aabb2d::new(gutter_position.0, gutter_collider.half_size());
+  
+        if let Some(collision) = collide_with_side(paddle_aabb, gutter_aabb) {
+          match collision {
+            Collision::Top => {
+              paddle_position.0.y = gutter_position.0.y
+                + gutter_collider.half_size().y
+                + paddle_collider.half_size().y;
+            }
+            Collision::Bottom => {
+              paddle_position.0.y = gutter_position.0.y
+                - gutter_collider.half_size().y
+                - paddle_collider.half_size().y;
+            }
+            _ => {}
+          }
+        }
+      }
+    }
+  }
+
+  fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::srgb(0.0, 0.0, 0.0)))
+        .add_systems(Startup, (
+            camera_setup, 
+            spawn_ball,
+            spawn_paddles,
+            spawn_gutters,
+        ))
+        .add_systems(
+            FixedUpdate,
+            (
+                handle_player_input,     
+                move_paddles,             
+                move_ball,                
+                project_positions,        
+                handle_collisions,        
+                constrain_paddle_position
+            )
+        )
+        .run();
+}
